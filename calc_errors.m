179899ad21a9cf557f756bce1a70d748dcbfa0a5
Madrant <a.polygaev@gmail.com>
2021-04-02 19:27:13 +0300

    calc_errors.m: Fixed error of MSE/RMSE dimensions

--- calc_errors.m
+++ calc_errors.m
@@ -2,13 +2,13 @@ function [error, abs_error, mse_array, rmse_array] = calc_errors(x, X)
     error = (x - X);
     abs_error = abs(error);
 
-    mse_array = zeros(length(error));
-    rmse_array = zeros(length(error));
+    mse_array = zeros(1, length(error));
+    rmse_array = zeros(1, length(error));
 
-    for n=1:length(error)
+    for n = 1:length(error)
         mse = mean(error(1:n).^2);
-        mse_array(n) = mse;
-        rmse_array(n) = sqrt(mse);
+        mse_array(1, n) = mse;
+        rmse_array(1, n) = sqrt(mse);
     end
 end
 
