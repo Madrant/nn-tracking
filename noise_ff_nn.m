a9e9983ed16c843821778302b30bc9e5fc2cb152
Madrant <a.polygaev@gmail.com>
2021-04-12 01:29:24 +0300

    A try to filter measurements noise using feedforward neural netowrk

---
+++ noise_ff_nn.m
@@ -0,0 +1,68 @@
+function net_outputs = noise_ff_nn(t, x, xn, sample_length, result_length, samples_div, hiddenSizes, trainFcn)
+    % Print options
+    fprintf("Samples: [%u:%u] Train sample div: %u\n", sample_length, result_length, samples_div);
+    fprintf("Network: Hidden: %u Train: '%s'\n", hiddenSizes, trainFcn);
+
+    % Create neural network
+    net = feedforwardnet(hiddenSizes, trainFcn);
+
+    % Prepare train data set
+    samples_num = round(length(x) / samples_div) - (sample_length + result_length - 1);
+
+    snr_values = [];
+    loops = length(snr_values) + 1;
+
+    samples = zeros(samples_num * loops, sample_length);
+    results = zeros(samples_num * loops, result_length);
+
+    for loop = 1: loops
+        if loop > length(snr_values)
+            xnt = xn;
+        else
+            snr = snr_values(loop);
+            xnt = awgn(x, snr, 'measured');
+        end
+
+        for n = 1 : samples_num
+            samples(n + (samples_num * (loop - 1)),:) = xnt(n: n + sample_length - 1);
+            results(n + (samples_num * (loop - 1)),:) = x(n + sample_length - 1:  n + sample_length + result_length - 2);
+        end
+    end
+
+    % Transpose test arrays to fit network inputs
+    samples = samples.';
+    results = results.';
+
+    % Configure network inputs:
+    net = configure(net, samples, results);
+    fprintf("net.inputs: %d\n", net.inputs{1}.size);
+
+    % Train network
+    [net, tr] = train(net, samples, results);
+
+    % Test network
+    test_step = 1;
+
+    real_data = zeros(samples_num, sample_length);
+    measurements = zeros(samples_num, sample_length);
+    net_outputs = zeros(samples_num, result_length);
+
+    for n = 1 : test_step: samples_num - sample_length
+        real = x(n: n + sample_length - 1);
+        measurement = xn(n: n + sample_length - 1);
+
+        net_output = net(measurement.').';
+
+        real_data(n,:) = real;
+        measurements(n,:) = measurement;
+        net_outputs(n,:) = net_output(1);
+    end
+
+    % Assess the performance of the trained network.
+    %
+    % The default performance function is mean squared error.
+    perf = perform(net, x, net_outputs);
+
+    % Convert column to row
+    net_outputs = net_outputs.';
+end
--- time_series_forecasting_test.m
+++ time_series_forecasting_test.m
@@ -24,7 +24,7 @@ phi = 0;
 A = floor(t);
 
 r = 0.01;
-snr = 20;
+snr = 1;
 
 xr1 = A .* sin(w * t + phi);
 xn1 = A .* (1 + normrnd(0, r)) .* sin(w * (1 + normrnd(0, r)) * t + phi*(1 + normrnd(0, r)));
@@ -43,9 +43,7 @@ fig_input = figure('Name', "Input data");
 hold on;
 plot(t, xr1, '-d');
 plot(t, xn1, '-x');
-plot(t, xr2, '--');
-plot(t, xn2, '--*');
-legend("Data 1", "Measurements 1", "Data 2", "Measurements 2");
+legend("Data 1", "Measurements 1");
 hold off;
 
 % Select input data
@@ -55,12 +53,13 @@ xn = xn1;
 fprintf("Time: [%f:%f:%f]\n", start_time, time_step, end_time);
 fprintf("SNR: %f\n", snr);
 
-fprintf("Measurements Max error: %f\n", max(abs(xr - xn)));
-fprintf("Measurements MSE:       %f\n", mean(xr - xn).^2);
-fprintf("Measurements RMSE:      %f\n", sqrt(mean(xr - xn).^2));
+fprintf("Measurements Max error:  %f\n", max(abs(xr - xn)));
+fprintf("Measurements Mean error: %f\n", mean(abs(xr - xn)));
+fprintf("Measurements MSE:        %f\n", mean(xr - xn).^2);
+fprintf("Measurements RMSE:       %f\n", sqrt(mean(xr - xn).^2));
 
 % NN options
-sample_length = 3;
+sample_length = 5;
 result_length = 1;
 samples_div = 1;
 
@@ -68,35 +67,41 @@ samples_div = 1;
 save_figure = 0;
 
 % Feedforward NN
-nn_outputs = ts_ff_nn(t, xt, xn, sample_length, result_length, samples_div, 4, 'trainrp');
-plot_results("FF NN 3", t, xt, xr, xn, nn_outputs, save_figure, sample_length);
+un_outputs = noise_ff_nn(t, xr, xn, sample_length, result_length, samples_div, 7, 'trainlm');
+plot_results("FF NN - Noise", t, xt, xr, xn, un_outputs, save_figure, sample_length);
+
+% sample_length = 5;
+% result_length = 1;
+
+% nn_outputs = ts_ff_nn(t, xt, un_outputs, sample_length, result_length, samples_div, 7, 'trainrp');
+% plot_results("FF NN - Prediction", t, xt, xr, un_outputs, nn_outputs, save_figure, sample_length);
 
 % LSTM NN
 %outputs = ts_lstm_nn(t, xt, xn, sample_length, result_length, samples_div);
 %plot_results("LSTM NN", t, xt, xr, xn, outputs, save_figure, sample_length);
 
 % Kalman filter
-kf_outputs = ts_kf(t, xt, xn);
-plot_results("KF", t, xt, xr, xn, kf_outputs, save_figure, 0);
+% kf_outputs = ts_kf(t, xt, xn);
+% plot_results("KF", t, xt, xr, xn, kf_outputs, save_figure, 0);
 
 % Extrapolation
-outputs = ts_extrap(t, xr, xn, 'linear', 3);
-plot_results("Extrapolation: Linear Points: 3", t, xt, xr, xn, outputs, save_figure, 3);
+% outputs = ts_extrap(t, xr, xn, 'linear', 3);
+% plot_results("Extrapolation: Linear Points: 3", t, xt, xr, xn, outputs, save_figure, 3);
 
-outputs = ts_extrap(t, xr, xn, 'spline', 3);
-plot_results("Extrapolation: Spline Points: 3", t, xt, xr, xn, outputs, save_figure, 3);
+% outputs = ts_extrap(t, xr, xn, 'spline', 3);
+% plot_results("Extrapolation: Spline Points: 3", t, xt, xr, xn, outputs, save_figure, 3);
 
-outputs = ts_extrap(t, xr, xn, 'spline', 5);
-plot_results("Extrapolation: Spline Points: 5", t, xt, xr, xn, outputs, save_figure, 5);
+% outputs = ts_extrap(t, xr, xn, 'spline', 5);
+% plot_results("Extrapolation: Spline Points: 5", t, xt, xr, xn, outputs, save_figure, 5);
 
 % Compare filter outputs
 
 % Plot input data:
-fig_outputs = figure('Name', "Filter outputs");
-hold on;
-plot(t, xr, '-');
-plot(t, xn, '-x');
-plot(t(sample_length + 1:length(t)), nn_outputs, '-o');
-plot(t, kf_outputs, '-*');
-legend("Data", "Measurements", "FF NN 3", "KF");
-hold off;
+% fig_outputs = figure('Name', "Filter outputs");
+% hold on;
+% plot(t, xr, '-');
+% plot(t, xn, '-x');
+% plot(t(sample_length + 1:length(t)), nn_outputs, '-o');
+% plot(t, kf_outputs, '-*');
+% legend("Data", "Measurements", "FF NN 3",
+% hold off;
