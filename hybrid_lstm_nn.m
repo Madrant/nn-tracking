e5386f5e18293e69cb63faa5cdbd58980eabf83d
Madrant <a.polygaev@gmail.com>
2021-04-20 17:15:32 +0300

    Added hybrid LSTM to both filter and approximate input data

---
+++ hybrid_lstm_nn.m
@@ -0,0 +1,127 @@
+function [net1_outputs, net2_outputs] = hybrid_lstm_nn(t, x, xn_train, xn_test, sl_array, rl_array, samples_div, hs_array, maxEpochs, hiddenType, snr_array)
+    % Print options
+    fprintf("Samples: [%.2f:%.2f] Train sample div: %.2f\n", sl_array(2), rl_array(2), samples_div);
+    fprintf("Network: Hidden: %u:%u Epochs: %u Type: '%s'\n", hs_array(1), hs_array(2), maxEpochs, hiddenType);
+
+    assert(hiddenType == "lstm" || hiddenType == "gru");
+
+    % Configure networks
+    net1_sample_length = sl_array(1);
+    net1_result_length = rl_array(1);
+
+    net2_sample_length = sl_array(2);
+    net2_result_length = rl_array(2);
+
+    if ~exist('maxEpochs', 'var')
+        maxEpochs = 100;
+    end
+
+    % Select hidden layer type
+    if hiddenType == "lstm"
+        net1_mainLayer = lstmLayer(hs_array(1));
+        net2_mainLayer = lstmLayer(hs_array(2));
+    end
+    if hiddenType == "gru"
+        net1_mainLayer = gruLayer(hs_array(1));
+        net2_mainLayer = gruLayer(hs_array(2));
+    end
+
+    net1_layers = [ ...
+        sequenceInputLayer(net1_sample_length)
+        net1_mainLayer
+        fullyConnectedLayer(net1_result_length)
+        regressionLayer
+    ];
+
+    net2_layers = [ ...
+        sequenceInputLayer(net2_sample_length)
+        net2_mainLayer
+        fullyConnectedLayer(net2_result_length)
+        regressionLayer
+    ];
+
+    net1_options = trainingOptions('sgdm', ... % sgdm, rmsprop, adam
+        'MaxEpochs', maxEpochs, ...
+        'MiniBatchSize', net1_sample_length, ...
+        'GradientThreshold', 1, ...
+        'Verbose', 0, ...
+        'Plots', 'none', ... % 'training-progress', 'none'
+        'InitialLearnRate',0.005, ...
+        'LearnRateSchedule','piecewise', ...
+        'LearnRateDropPeriod',125, ...
+        'LearnRateDropFactor',0.2, ...
+        'Shuffle', 'never');
+
+    net2_options = trainingOptions('sgdm', ... % sgdm, rmsprop, adam
+        'MaxEpochs', maxEpochs, ...
+        'MiniBatchSize', net2_sample_length, ...
+        'GradientThreshold', 1, ...
+        'Verbose', 0, ...
+        'Plots', 'none', ... % 'training-progress', 'none'
+        'InitialLearnRate',0.005, ...
+        'LearnRateSchedule','piecewise', ...
+        'LearnRateDropPeriod',125, ...
+        'LearnRateDropFactor',0.2, ...
+        'Shuffle', 'never');
+    
+    % Additional training options:
+    %
+    %'GradientThreshold', 1, ...
+    %'Momentum', 0.9, ... % only for sgdm
+    %'InitialLearnRate', 0.005, ...
+    %'LearnRateSchedule', 'piecewise', ...
+    %'LearnRateDropPeriod', 125, ...
+    %'LearnRateDropFactor', 0.2, ...        
+    %'Shuffle', 'never', ... % once, never, every-epoch
+    
+    % Prepare train data set for net1
+    [net1_samples, net1_results] = prepare_train_data(x, xn_train, net1_sample_length, net1_result_length, 0, samples_div, snr_array);
+
+    % Train net1
+    net1 = trainNetwork(net1_samples.', net1_results.', net1_layers, net1_options);
+
+    % Get net1 output to train net2
+    [net1_test_samples, net1_test_results] = prepare_train_data(x, xn_train, net1_sample_length, net1_result_length, 0, 1, snr_array);
+    net1_outputs = test_network(net1, net1_test_samples, net1_result_length, hiddenType);
+
+    net1_outputs = net1_outputs.';
+
+    fprintf("net1 output:\n")
+    fprintf("inputs:  "); disp(size(net1_test_samples));
+    fprintf("outputs: "); disp(size(net1_outputs));
+
+    % Generate train data for second network
+    loops = length(snr_array) + 1;
+
+    net2_test_samples = [];
+    net2_test_results = [];
+    
+    for n = 1:loops
+        from = length(x) * (n - 1) + 1;
+        to = length(x) * n;
+
+        [test_samples, test_results] = prepare_train_data(...
+            x, net1_outputs(from:to), net2_sample_length, net2_result_length);
+
+        net2_test_samples = [net2_test_samples; test_samples];
+        net2_test_results = [net2_test_results; test_results];
+    end
+
+    fprintf("net2 train data:\n");
+    fprintf("inputs:  "); disp(size(net2_test_samples));
+    fprintf("outputs: "); disp(size(net2_test_results));
+
+    % Train second network
+    net2 = trainNetwork(net2_test_samples.', net2_test_results.', net2_layers, net2_options);
+
+    % Process data by using net1 and net2 together
+    [test_samples, test_results] = prepare_train_data(x, xn_test, net1_sample_length, net1_result_length, 0);
+
+    % Process noise by net1
+    net1_outputs = test_network(net1, test_samples, net1_result_length, hiddenType);
+
+    [test_samples, test_results] = prepare_train_data(x, net1_outputs, net2_sample_length, net2_result_length);
+
+    % Approximate net1 output by net2
+    net2_outputs = test_network(net2, test_samples, net2_result_length, hiddenType);
+end
--- time_series_forecasting_test.m
+++ time_series_forecasting_test.m
@@ -10,19 +10,11 @@ end_time = 10;
 % Initialize random number generator
 rng(12345, 'combRecursive');
 
-% Generate training data (filter model)
-t = start_time:time_step:end_time;
-
-w = 2 * pi;
-phi = 0;
-A = floor(t);
-
-xt = A .* sin(w * t + phi);
-xt = normalize(xt, 'range');
-
 % Generate test data (real target position)
 r = 0.01;
 snr = 5;
+
+t = start_time:time_step:end_time;
 
 % Data set 1 (xr1, xr2)
 w = 2 * pi;
@@ -83,9 +75,9 @@ fprintf("Measurements MSE:        %f\n", mean(xr - xn).^2);
 fprintf("Measurements RMSE:       %f\n", sqrt(mean(xr - xn).^2));
 
 % NN options
-sample_length = 3;
+sample_length = 5;
 result_length = 1;
-samples_div = 1;
+samples_div = 1.5;
 
 hiddenSize = 7;
 maxEpochs = 100;
@@ -96,10 +88,11 @@ save_figure = 0;
 % Enable/disable some various networks in test
 en_nn_ff_ns = 0;
 en_nn_ff_ts = 0;
-en_nn_lstm_ns = 1;
-en_nn_lstm_ts = 1;
+en_nn_lstm_ns = 0;
+en_nn_lstm_ts = 0;
 en_nn_gru_ns = 0;
 en_nn_gru_ts = 0;
+en_nn_hybrid = 1;
 
 for sample_length = [sample_length] %[1 3 5]
 for hiddenSize = [hiddenSize] %[4, 5, 7, 10]
@@ -159,44 +152,21 @@ if en_nn_gru_ts
     res_nn_gru_ts = nn_outputs;
 end
 
-if 0
-    sample_length = 1;
-    hiddenSize = 5;
-
-    disp(size(xn_test));
-
-    snr_array = [5];
-
-    name = sprintf("LSTM NN - Noise Hs %u Samples %u Div %.2f", hiddenSize, sample_length, samples_div);
-    un_outputs = noise_lstm_nn(t, xr_train, xn_train, xn_test, sample_length, result_length, samples_div, hiddenSize, maxEpochs, "lstm", snr_array);
-    plot_results(name, t, xr_train, xr_test, xn_test, un_outputs, save_figure, 0);
-
-    disp(size(un_outputs));
-
-    sample_length = 1;
-    hiddenSize = 5;
-
-    snr_array = [];
-
-    name = sprintf("LSTM NN - Prediction Hs %u Samples %u Div %.2f", hiddenSize, sample_length, samples_div);
-    nn_outputs = noise_lstm_nn2(t, xr_train, un_outputs, un_outputs, sample_length, result_length, samples_div, hiddenSize, maxEpochs, "lstm", snr_array);
-    plot_results(name, t, xr_train, xr_test, un_outputs, nn_outputs, save_figure, 0);
-
-    disp(size(nn_outputs));
-end
-
-if 0
-    sample_length = 1;
-    samples_div = 1;
-    hiddenSize = 5;
-
-    disp(size(xn_test));
-
+if en_nn_hybrid
+    sl_array = [1 5]; t_skip = abs(sl_array(1) - sl_array(2));
+    rl_array = [1 1];
+    hs_array = [5 11];
     snr_array = [5 5 5];
+    samples_div = 1;
 
-    name = sprintf("LSTM NN - Noise Hs %u Samples %u Div %.2f", hiddenSize, sample_length, samples_div);
-    [nn1_outputs, nn2_outputs] = hybrid_lstm_nn(t, xr_train, xn_train, xn_test, sample_length, result_length, samples_div, hiddenSize, maxEpochs, "lstm", snr_array);
-    plot_results(name, t, xr_train, xr_test, xn_test, nn2_outputs, save_figure, 4);
+    name = sprintf("Hybrid LSTM NN - Noise Hs %u Samples %u Div %.2f", hiddenSize, sample_length, samples_div);
+
+    [net1_outputs, net2_outputs] = hybrid_lstm_nn(...
+        t, xr_train, xn_train, xn_test, ...
+        sl_array, rl_array, samples_div, ...
+        hs_array, maxEpochs, "lstm", snr_array);
+
+    plot_results(name, t, xr_train, xr_test, xn_test, net2_outputs, save_figure, t_skip);
 end
 
 end % hiddenSize
