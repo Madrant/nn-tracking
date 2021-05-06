56f0e7d6bd882e5dd5d55be49f132a613017ad9b
Madrant <a.polygaev@gmail.com>
2021-05-06 15:20:34 +0300

    Fixed random noise generation

--- gen_sin.m
+++ gen_sin.m
@@ -2,14 +2,14 @@ function [xr, xn] = gen_sin(t, A, w, phi, r, snr)
     if ~exist('r', 'var')
         r = 0;
     end
-    
+
     noise_data = true;
     if ~exist('snr', 'var') || snr == 0
         noise_data = false;
     end
-    
+
     xr = A .* sin(w * t + phi);
-    xn = A .* (1 + normrnd(0, r)) .* sin(w * (1 + normrnd(0, r)) * t + phi*(1 + normrnd(0, r)));
+    xn = A .* (1 + normrnd(0, r)) .* sin(w * t + phi);
 
     if noise_data
         xn = awgn(xn, snr, 'measured');
