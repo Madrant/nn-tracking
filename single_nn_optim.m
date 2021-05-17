9d9818e97a3fdcb68bbaf9025e727a483557dd41
Madrant <a.polygaev@gmail.com>
2021-05-17 17:11:44 +0300

    single_nn_optim.m: Type, Size, Activation, Dropout parameters can be excluded from optimization. Predict offset set to 1 from 0.

--- single_nn_optim.m
+++ single_nn_optim.m
@@ -33,6 +33,10 @@ global xr_train;
 global xn_train;
 global xr_test;
 global xn_test;
+global result_length;
+global samples_div;
+global predict_offset;
+global snr_array;
 
 xr_train = xr1;
 xn_train = xn1;
@@ -40,35 +44,67 @@ xn_train = xn1;
 xr_test = xr2;
 xn_test = xn2;
 
-global result_length;
-global samples_div;
 result_length = 1;
+predict_offset = 1;
 samples_div = 1.5;
 
-global snr_array;
-snr_array = [snr snr snr snr snr];
+snr_array = [snr snr snr];
 
 save_figure = false;
+
+fprintf("Samples: %u Predict: %u Div: %.2f SNR size: %u\n", length(xr1), ...
+    predict_offset, samples_div, length(snr_array));
+
+% Plot input data:
+if true
+    fig_input = figure('Name', "Input data");
+    tiledlayout(2, 1);
+
+    nexttile;
+    hold on;
+    plot(t, xr_train, '-');
+    plot(t, xn_train, '-x');
+    legend("Train Data", "Train Measurements");
+    hold off;
+
+    nexttile;
+    hold on;
+    plot(t, xr_test, '-');
+    plot(t, xn_test, '-x');
+    legend("Test Data", "Test Measurements");
+    hold off;
+end
+
+print_data_stats(xr_train, xn_train);
+print_data_stats(xr_test, xn_test);
 
 % Define variables to optimize:
 %
 % See also:
 % https://www.mathworks.com/help/deeplearning/ref/trainingoptions.html
-maxLayers = 5;
+minLayers = 2;
+maxLayers = 3;
 
 optimVars = [
     optimizableVariable('sequenceLength', [1 100], 'Type', 'integer', 'Optimize', true)
 
-    optimizableVariable('numLayers', [2 maxLayers], 'Type', 'integer', 'Optimize', true)
+    optimizableVariable('numLayers', [minLayers maxLayers], 'Type', 'integer', 'Optimize', true)
 
-    optimizableVariable('gradientThreshold', [0.01 10], 'Transform', 'log', 'Optimize', true)
+    optimizableVariable('gradientThreshold', [0.001 10], 'Transform', 'log', 'Optimize', true)
     optimizableVariable('initialLearnRate', [0.0001 1], 'Transform', 'log', 'Optimize', true)
 ];
 
-global optimize_type; optimize_type = false; % LSTM by default
+global optimize_type; optimize_type = false;
+global optimize_type_default; optimize_type_default = "lstm";
+
 global optimize_size; optimize_size = true;
+global optimize_size_default; optimize_size_default = 10;
+
 global optimize_act; optimize_act = true;
+global optimize_act_default; optimize_act_default = "none";
+
 global optimize_drop; optimize_drop = true;
+global optimize_drop_default; optimize_drop_default = 0.00;
 
 % Optimize layers structure cccording to maxLayers
 for n = 1:maxLayers
@@ -90,7 +126,7 @@ end
 disp(optimVars);
 
 BayesObject = bayesopt(make_validation_fcn, optimVars,  ...
-    'MaxObjectiveEvaluations', 100, ...
+    'MaxObjectiveEvaluations', 30, ...
     'MaxTime', 14*60*60, ...
     'IsObjectiveDeterministic', false, ...
     'UseParallel', false);
@@ -113,7 +149,7 @@ disp(opt);
 
 sample_length = 1;
 
-[test_samples, test_results] = prepare_train_data(xr_test, xn_test, sample_length, result_length, 0);
+[test_samples, test_results] = prepare_train_data(xr_test, xn_test, sample_length, result_length, predict_offset);
 X = test_network(net, test_samples, result_length, "lstm");
 
 [xrs, X] = align_data(xr_test, X);
@@ -121,7 +157,7 @@ X = test_network(net, test_samples, result_length, "lstm");
 [error, abs_error, mse_array, rmse_array, max_error, mean_error, mse, rmse] = calc_errors(xrs, X);
 
 fprintf("SL: %u HS: %u ME: %f MSE: %f\n", opt.sequenceLength, opt.hs1, mean_error, mse);
-plot_results("Best NN configuration", t, xr_test, xr_test, xn_test, X, save_figure, samples_div);
+plot_results("Best NN configuration", t, xr_train, xr_test, xn_test, X, save_figure, samples_div);
 
 function ObjFcn = make_validation_fcn()
 ObjFcn = @validation_fcn;
@@ -144,22 +180,35 @@ ObjFcn = @validation_fcn;
         global optimize_size;
         global optimize_act;
         global optimize_drop;
-        
+
+        global optimize_type_default;
+        global optimize_size_default;
+        global optimize_act_default;
+        global optimize_drop_default;
+
         for n = 1:optimVars.numLayers
             if optimize_type
                 ht_array(1, end + 1) = eval(sprintf("optimVars.ht%u", n));
+            else
+                ht_array(1, end + 1) = optimize_type_default;
             end
 
             if optimize_size
                 hs_array(1, end + 1) = eval(sprintf("optimVars.hs%u", n));
+            else
+                hs_array(1, end + 1) = optimize_size_default;
             end
 
             if optimize_act
                 act_array(1, end + 1) = eval(sprintf("optimVars.act%u", n));
+            else
+                act_array(1, end + 1) = optimize_act_default;
             end
 
             if optimize_drop
                 drop_array(1, end + 1) = eval(sprintf("optimVars.drop%u", n));
+            else
+                drop_array(1, end + 1) = optimize_drop_default;
             end
         end
 
@@ -194,11 +243,6 @@ ObjFcn = @validation_fcn;
         %'LearnRateDropFactor', 0.2, ...        
         %'Shuffle', 'never', ... % once, never, every-epoch
 
-        % Prepare train data
-        %
-        % sample_length = sample_length;
-        % [samples, results] = prepare_train_data(x, xn_train, sample_length, result_length, 0, samples_div, snr_array);
-
         % train_samples = samples; train_results = results;
         sample_length = 1;
         global xr_train;
@@ -206,9 +250,10 @@ ObjFcn = @validation_fcn;
         global xr_test;
         global xn_test;
         global result_length;
+        global predict_offset;
         global samples_div;
         global snr_array;
-        [samples, results] = prepare_train_data(xr_train, xn_train, sample_length, result_length, 0, samples_div, snr_array);
+        [samples, results] = prepare_train_data(xr_train, xn_train, sample_length, result_length, predict_offset, samples_div, snr_array);
 
         % Convert arrays to fit network inputs
         train_samples = samples.';
@@ -224,10 +269,10 @@ ObjFcn = @validation_fcn;
         net = trainNetwork(train_samples, train_results, layers, options);
 
         % Test network
-        [test_samples, test_results] = prepare_train_data(xr_test, xn_test, sample_length, result_length, 0);
+        [test_samples, test_results] = prepare_train_data(xr_test, xn_test, sample_length, result_length, predict_offset);
         X = test_network(net, test_samples, result_length, "lstm");
 
-        [xrs, X]= align_data(xr_test, X);
+        [xrs, X] = align_data(xr_test, X);
 
         [error, abs_error, mse_array, rmse_array, max_error, mean_error, mse, rmse] = calc_errors(xrs, X);
 
